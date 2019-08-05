07e491fd4fda7faf4d5b8640362fc6585e7c6e3c
Takashi Ishio <ishio@is.naist.jp>
2019-08-05 22:29:47 +0900

    Added Token printer

---
+++ src/main/java/jp/naist/se/codehash/comparison/FileTokenizerMain.java
@@ -0,0 +1,51 @@
+package jp.naist.se.codehash.comparison;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import com.fasterxml.jackson.core.JsonFactory;
+import com.fasterxml.jackson.core.JsonGenerator;
+
+import jp.naist.se.codehash.FileType;
+import jp.naist.se.codehash.TokenReader;
+
+public class FileTokenizerMain {
+
+	public static void main(String[] args) {
+		JsonFactory f = new JsonFactory();
+		try (JsonGenerator gen = f.createGenerator(System.out)) {
+			gen.writeStartObject();
+			gen.writeArrayFieldStart("Files");
+			for (String filename: args) {
+				FileType t = FileType.getFileTypeFromName(filename);
+				if (FileType.isSupported(t)) {
+					gen.writeStartObject();
+					gen.writeStringField("Type", t.name());
+					gen.writeStringField("Name", filename);
+					gen.writeArrayFieldStart("Tokens");
+					try (FileInputStream input = new FileInputStream(filename)) {
+						TokenReader r = FileType.createReader(t, input);
+						while (r.next()) {
+							gen.writeStartObject();
+							gen.writeStringField("Text", r.getText());
+							gen.writeStringField("NormalizedText", r.getNormalizedText());
+							gen.writeNumberField("Line", r.getLine());
+							gen.writeNumberField("CharPositionInLine", r.getCharPositionInLine());
+							gen.writeEndObject();
+						}
+						gen.writeEndArray();
+					} catch (IOException e) {
+						gen.writeEndArray();
+						gen.writeStringField("IOException", e.getMessage());
+					}
+					gen.writeEndObject();
+				}
+			}
+			gen.writeEndArray();
+			gen.writeEndObject();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
